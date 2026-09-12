b40ba0e5f7e5257385ade8657ffc8a11a175118c
Liza Shrestha <shresthaliza4@gmail.com>
2026-09-12 12:02:38 +0545

    added floating image

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+app = QApplication(sys.argv)
+
+penguin =QLabel()
+
+image = QPixmap("assets/alternative-temp.png")
+penguin.setPixmap(image)
+
+penguin.setWindowFlags(
+    Qt.FramelessWindowHint |
+    Qt.WindowStaysOnTopHint |
+    Qt.Tool
+)
+
+penguin.setAttribute(Qt.WA_TranslucentBackground)
+
+penguin.show()
+
+sys.exit(app.exec_())
