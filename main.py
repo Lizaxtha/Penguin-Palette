9ce15cdc04e5332446d7ead94503c218643625db
Liza Shrestha <shresthaliza4@gmail.com>
2026-09-13 15:50:36 +0545

    made the penguin dragable

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class Penguin(QLabel):
         self.normal_image = QPixmap("assets/Penguin-default.png")
         self.palette_image =QPixmap("assets/Penguin-paints.png")
 
+        self.drag_position = None
+
         self.show_normal_penguin()
 
         self.setWindowFlags(
@@ -31,9 +33,30 @@ class Penguin(QLabel):
         self.setPixmap(self.palette_image)
         self.adjustSize()
 
+# "use it later to center all images if not centered"
+        # center =  self.geometry().center()  
+        # self.move(
+            # center.x() - self.width // 2,
+            # center.y() - self.height //2
+        # )
+
     def mousePressEvent(self, event):
        if event.button() == Qt.LeftButton:
+
+           self.drag_position = event.globalPos()
+           
+
            self.show_palette_penguin()
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() & Qt.LeftButton:
+
+            self.move(
+                event.globalPos() - self.drag_position
+            )
+
+    def mouseReleaseEvent(self, event):
+        self.drag_position = None
     
 app = QApplication(sys.argv)
 
